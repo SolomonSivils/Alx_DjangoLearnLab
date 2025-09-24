c66e6d94cb1f7de51c3761c1b19cf0fb0c61f09b
Eric Macharia <iriqmash21@gmail.com>
2025-09-24 21:47:54 +0300

    task1

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 
     # Path for the Author model
     path('authors/', AuthorListCreateView.as_view(), name='author-list-create'),
