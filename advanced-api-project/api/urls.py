fc650fcb7e162a48324846affb19ecf16b66de08
Eric Macharia <iriqmash21@gmail.com>
2025-09-24 21:45:58 +0300

    task1

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,15 +2,17 @@
 This module defines the URL patterns for the API application.
 """
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView, AuthorListCreateView
 
 # Define URL patterns for the API endpoints
 urlpatterns = [
-    # Path for listing all books and creating a new book.
-    # The .as_view() method is used to convert the class-based view into a callable function.
-    path('books/', BookListView.as_view(), name='book-list-create'),
-
-    # Path for retrieving, updating, or deleting a single book by its primary key (pk).
-    # The <int:pk> part is a dynamic URL parameter that captures the book's ID.
+    # Paths for the Book model
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+
+    # Path for the Author model
+    path('authors/', AuthorListCreateView.as_view(), name='author-list-create'),
 ]
