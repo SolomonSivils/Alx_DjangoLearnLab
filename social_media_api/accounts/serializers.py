14ec8861c10ddf25ab85a24242de4cd2fc61c68a
Eric Macharia <iriqmash21@gmail.com>
2025-10-19 14:40:06 +0300

    Task 0

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,10 +1,11 @@
 # accounts/serializers.py
 from rest_framework import serializers
 from .models import CustomUser
+from rest_framework.authtoken.models import Token
 
 # Serializer for User Registration
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = CustomUser
